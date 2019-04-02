215894730d13c532ceff22f6b8cc0a4b7a7a47cb
ofifphantom <pnegruijie_19@hotmail.com>
2019-04-02 08:03:14 +0800

    2019-04-02

--- src/com/jl/app/api/OrderApi.java
+++ src/com/jl/app/api/OrderApi.java
@@ -5,6 +5,17 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -1495,6 +1506,60 @@ public class OrderApi {
 			}
 		}
 		return success;
+	}
+	
+	
+	private String insertOrder2JLPSI(JSONObject json) {
+
+        HttpClient client = new DefaultHttpClient();
+        String URL = "http://39.105.115.214:8001/JLPSI/sales/salesOrder/addSalesOrder?planOrTable=2";
+        HttpPost post = new HttpPost(URL);
+        post.setHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Basic YWRtaW46");
+        String result = "";
+        
+        try {
+
+            StringEntity s = new StringEntity(json.toString(), "utf-8");
+            s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,
+                    "application/json"));
+            post.setEntity(s);
+
+            // 发送请求
+            HttpResponse httpResponse = client.execute(post);
+
+            // 获取响应输入流
+            InputStream inStream = httpResponse.getEntity().getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    inStream, "utf-8"));
+            StringBuilder strber = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null)
+                strber.append(line + "\n");
+            inStream.close();
+
+            result = strber.toString();
+            System.out.println(result);
+            
+            if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                
+                    System.out.println("请求服务器成功，做相应处理");
+                
+            } else {
+                
+                System.out.println("请求服务端失败");
+                
+            }
+            
+
+        } catch (Exception e) {
+            System.out.println("请求异常");
+            throw new RuntimeException(e);
+        }
+
+        return result;
+		
+		
 	}
 
 }
